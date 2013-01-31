b33425dbf0a34c8ae0650c35bb8e90845e474347
Gabriel <gabriel@abrightconcept.com>
2013-01-31 03:17:50 -0500

    Move application-specific directories under one main application directory, and update .gitignore and paths.php to match

--- system/config/paths.php
+++ system/config/paths.php
@@ -28,29 +28,35 @@ else
 }
 
 // Set up paths derived from the root
-define('MODEL_PATH', FILE_ROOT_PATH . '/models');
-define('VIEW_PATH', FILE_ROOT_PATH . '/views');
-define('CONTROLLER_PATH', FILE_ROOT_PATH . '/controllers');
-define('CORE_PATH', FILE_ROOT_PATH . '/system/core');
+define('APPLICATION_PATH', FILE_ROOT_PATH . '/application');
+define('SYSTEM_PATH', FILE_ROOT_PATH . '/system');
+define('MODEL_PATH', APPLICATION_PATH . '/models');
+define('VIEW_PATH', APPLICATION_PATH . '/views');
+define('CONTROLLER_PATH', APPLICATION_PATH . '/controllers');
+define('CORE_PATH', SYSTEM_PATH . '/core');
 define('LIBRARY_PATH', FILE_ROOT_PATH . '/lib');
-define('LOGS_PATH', FILE_ROOT_PATH . '/logs');
+define('LOGS_PATH', APPLICATION_PATH . '/logs');
 
 // Set up paths that depend upon development environment mode
 if (IS_MODE_PRODUCTION)
 {
 	define('HTTP_ROOT_PATH', 'http://' . DOMAIN_NAME);
-	define('PUBLIC_PATH', HTTP_ROOT_PATH . '/public');
-	define('JSON_PATH', FILE_ROOT_PATH . '/json');
-	define('XML_PATH', FILE_ROOT_PATH . '/xml');
+	define('HTTP_APPLICATION_PATH', HTTP_ROOT_PATH . '/application');
 	
-	define('JS_PATH', PUBLIC_PATH . '/js');
-	define('CSS_PATH', PUBLIC_PATH . '/css');
+	define('PUBLIC_PATH', HTTP_APPLICATION_PATH . '/public');
+	define('JSON_PATH', APPLICATION_PATH . '/json');
+	define('XML_PATH', APPLICATION_PATH . '/xml');
+	
+	define('JS_PATH', HTTP_APPLICATION_PATH . '/js');
+	define('CSS_PATH', HTTP_APPLICATION_PATH . '/css');
 }
 else
 {
 	define('HTTP_ROOT_PATH', 'http://localhost/' . DOMAIN_NAME);
-	define('PUBLIC_PATH', HTTP_ROOT_PATH . '/dev');
-	define('DEVELOPMENT_PATH', FILE_ROOT_PATH . '/dev');
+	define('HTTP_APPLICATION_PATH', HTTP_ROOT_PATH . '/application');
+	
+	define('PUBLIC_PATH', HTTP_APPLICATION_PATH . '/dev');
+	define('DEVELOPMENT_PATH', APPLICATION_PATH . '/dev');
 	define('DEVELOPMENT_LIBRARY_PATH', DEVELOPMENT_PATH . '/lib');
 	define('TESTS_PATH', DEVELOPMENT_PATH . '/tests');
 	define('JSON_PATH', DEVELOPMENT_PATH . '/json');
@@ -58,8 +64,8 @@ else
 	define('LESS_IN_PATH', DEVELOPMENT_PATH . '/less');
 	define('LESS_OUT_PATH', DEVELOPMENT_PATH . '/css');
 	
-	define('JS_PATH', HTTP_ROOT_PATH . '/dev/js');
-	define('CSS_PATH', HTTP_ROOT_PATH . '/dev/css');
+	define('JS_PATH', HTTP_APPLICATION_PATH . '/dev/js');
+	define('CSS_PATH', HTTP_APPLICATION_PATH . '/dev/css');
 }
 
 // Set up other paths
@@ -68,6 +74,6 @@ define('APP_VIEW_PATH', VIEW_PATH . '/_app');
 define('APP_CONTROLLER_PATH', CONTROLLER_PATH . '/app');
 define('TEMPLATE_PATH', VIEW_PATH . '/_template');
 define('IMAGES_PATH', PUBLIC_PATH . '/images');
-define('ERROR_HANDLER_PAGE_PATH', HTTP_ROOT_PATH . '/views/_template/error.html');
+define('ERROR_HANDLER_PAGE_PATH', HTTP_APPLICATION_PATH . '/views/_template/error.html');
 
 /* EOF config/paths.php */
